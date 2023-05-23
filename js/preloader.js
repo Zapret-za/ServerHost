2dfbfc7d4db0c535abf16f77f8230ff79dcee984
Zapret-za <109032826+Zapret-za@users.noreply.github.com>
2023-05-23 23:06:32 +0300

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var preloaders = document.getElementsByClassName("preloader");
-  if (preloaders.length > 0) {
-    var preloader = preloaders[0];
-    preloader.style.display = "flex";
+  var preloader = document.getElementsByClassName("preloader");
+  if (preloader.length > 0) {
+    preloader[0].style.display = "flex";
   }
 
   var images = document.getElementsByTagName("img");
-  var imagesLoaded = 0;
-  var totalImages = images.length;
+  var svgs = document.getElementsByTagName("svg");
+  var totalFiles = images.length + svgs.length;
+  var filesLoaded = 0;
 
-  for (var i = 0; i < totalImages; i++) {
+  var onFileLoad = function() {
+    filesLoaded++;
+
+    if (filesLoaded === totalFiles) {
+      preloader[0].style.display = "none";
+      document.body.style.visibility = "visible";
+    }
+  };
+
+  for (var i = 0; i < images.length; i++) {
     var image = new Image();
-    image.onload = function() {
-      imagesLoaded++;
-
-      if (imagesLoaded === totalImages) {
-        preloader.style.display = "none";
-        document.body.style.visibility = "visible";
-      }
-    };
-
+    image.onload = onFileLoad;
     image.src = images[i].src;
   }
+
+  for (var j = 0; j < svgs.length; j++) {
+    svgs[j].addEventListener("load", onFileLoad);
+  }
 });
-
-
-
